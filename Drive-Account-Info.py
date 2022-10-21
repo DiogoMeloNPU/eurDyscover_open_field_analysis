fac030a6382c2b062d7e0088da395849a38d8b95
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2022-10-21 14:07:23 +0100

    Learning how to use Google Drive API

---
+++ Drive-Account-Info.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+from Google import Create_Service
+import pandas as pd
+
+CLIENT_SECRET_FILE = 'client_secrets.json'
+API_NAME = 'drive'
+API_VERSION = 'v3'
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+response = service.about().get(fields='*').execute()
+#pprint(response)
+
+for k, v in response.get('storageQuota').items():
+    print('{0}: {1:.2f}MB'.format(k, int(v) / 1024**2))
+    print('{0}: {1:.2f}GB'.format(k, int(v) / 1024**3))

916c4ac7f5cdb5080cc46789afde6ed77e69788d
DiogoMeloNPU <113690249+DiogoMeloNPU@users.noreply.github.com>
2022-10-26 09:43:39 +0100

    Add files via upload
    Matlab code to create simpler .mat files that can be opened in python.

---
+++ Sources2D_to_simple_mat.m
@@ -0,0 +1,21 @@
+function Sources2D_to_simple_mat(file_name, file_path)
+    % CNMFe outputs .mat files that store a matlab object of type Sources2D which can
+    % only be opened in matlab with the Sources2D class definition file in the matlab 
+    % path. This function converts these files into a basic .mat file that can be 
+    % opened in Python using scipy.io.loadmat.
+    % (c) Thomas Akam 2019. GPL-3 open source licence.
+
+    load(file_name, 'neuron')
+
+    A = neuron.A;
+    C = neuron.C;
+    S = neuron.S;
+    C_raw = neuron.C_raw;
+    image_size = size(neuron.Cn);
+    
+    %save('data.mat','A', 'C', 'S', 'C_raw', 'image_size', 'file_name', '-v6')
+    %save(file_path,'data.mat','A', 'C', 'S', 'C_raw', 'image_size', 'file_name', '-v6')
+    str = ["simpler", file_name]
+    newStr = char(join(str, '_'))
+    save(fullfile(file_path, newStr))
+end

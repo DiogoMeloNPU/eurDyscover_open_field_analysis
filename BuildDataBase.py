0bd7a96afc8d9b226ee1a5b7c21515667f539380
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2022-10-28 14:03:23 +0100

    Create file to build a dataframe (database) containing all the paths to the files available

---
+++ BuildDataBase.py
@@ -0,0 +1,15 @@
+#In this file I will be using multiindexing to structure a database with all the files available for the Dystonia Project
+
+#Columns
+#D1/D2
+#Sham/Lesioned
+#WT/DYT
+#BL1/BL2/W01/W03/W06/W09
+#mice1/mice2/mice3/...
+
+#Rows
+#neuron.mat
+#AccelData.csv
+#DLC.csv
+#VideoProcessed.avi
+#...

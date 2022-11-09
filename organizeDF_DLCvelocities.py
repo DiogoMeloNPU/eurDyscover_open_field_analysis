4731ae2bb5e4b0a7799e060e896ec898a95544b9
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2022-11-09 14:41:38 +0000

    wrote some comments to orient the code writing

---
+++ organizeDF_DLCvelocities.py
@@ -0,0 +1,11 @@
+#leave this one to the end, since...
+
+#in order to run this, it is necessary to have the file containing the organized DLC coordinate predictions as well as the respective timestamps
+
+#get the paths for the files containing the organized DLC coordinate predictions
+
+#open the file as a data frame
+
+#create a function to calculate the x and y velocity components as well as the vector magnitude
+#create a new data frame containing only the vector magnitude of each bodypart as well as the timestamps (eliminate the likelihoods)
+#

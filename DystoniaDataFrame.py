024c79282242a96d24b8a08bb605bd3fa1a42daf
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2022-11-10 10:18:53 +0000

    Started using pkl instead of csv

--- DystoniaDataFrame.py
+++ DystoniaDataFrame.py
@@ -136,13 +136,12 @@ for parent in parentFoldersOtherFiles:
                             dystoniaFilesDF['VideoProcessed.avi'][match.index[0]] = currentFile[2]
                             print('The following file path was added to the "VideoProcessed.avi" column: {}\n'.format(currentFile[2]))
 
+#show the dataframe
 print(dystoniaFilesDF)
 
-#finally, rearrange the dataframe for multiindexing access
-
-#save the dataframe as a csv file in google drive
-path2saveDF = "E:\\.shortcut-targets-by-id\\1MH0egFqTqTToPE-wxCs7mDWL48lVKqDB\\EurDyscover\\Organized_data_JAS\\dystoniaFilesDF.csv"
-dystoniaFilesDF.to_csv(path2saveDF)
+#save the dataframe as a pickle file in google drive
+path2saveDF = "E:\\.shortcut-targets-by-id\\1MH0egFqTqTToPE-wxCs7mDWL48lVKqDB\\EurDyscover\\Organized_data_JAS\\dystoniaFilesDF.pkl"
+dystoniaFilesDF.to_pickle(path2saveDF)
 
 #while dystoniaFilesDF is incomplete, just save it to the Desktop to check if is is being created correctly
 #DesktopPath = "C:\\Users\\user\\Desktop\\DystoniaDataBase.csv"

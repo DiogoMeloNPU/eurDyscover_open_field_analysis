c4193bbf18d51513715577acf1dda56d95fada51
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2022-12-11 23:27:26 +0000

    Created a module to get the missing frames from metadata files

---
+++ checkMissingFrames_MetaData.py
@@ -0,0 +1,28 @@
+#This module opens metadata files containing information on missing frames and 
+#...retrives the respective missing frames for correction of C_raw data
+
+import os
+from os import walk
+from xml.dom import minidom
+
+#parent file path
+file_path = "E:\\.shortcut-targets-by-id\\1MH0egFqTqTToPE-wxCs7mDWL48lVKqDB\\EurDyscover\\Imaging"
+
+def retrieveMissingFrames(path_xml):
+    '''
+    This function opens a .xml file and returns an array corresponding to the missing frames.
+    '''
+    #parse the xml file by name
+    file = minidrom.parse(path_xml)
+
+    
+
+    return missingFrames
+
+
+for file_path, subdirs, files in os.walk(file_path):
+    for name in files:
+        print(os.path.join(file_path, name))
+
+
+#create a new column in the dystoniaFilesDF with the array of missing frames

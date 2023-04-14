be00bb1be5ea8e106fbb44c41e535d6860b50c3c
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2023-04-14 18:06:19 +0100

    Created a file (pseudo-code) to study the opening of the hind paws while standing still

---
+++ distancePaws.py
@@ -0,0 +1,9 @@
+# per session
+
+# compute the amount of time an animal is standing still
+
+# compute distance between the digital tips of the hind paws
+
+# compute distance between the heels of the hind paws
+
+# save dataframe containing results per mice

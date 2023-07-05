cefa7074a34b6bb642b28db36eef69e2519df1ef
Diogo Melo <diogo.melo@research.fchampalimaud.org>
2023-07-05 13:04:38 +0100

    Organizing session files and creating general purpose module folder

---
+++ data_analysis/event_detection/movement_initiation_accelerometer.py
@@ -0,0 +1,25 @@
+from general_purpose_modules
+ 
+# load dystonia database
+dystonia_database = gp.load_dystonia_database()
+print(dystonia_database)
+
+# access all acceleration files
+
+# establish a treshold for movement initiation using acceleration (accelerometer palced in the head)
+
+# study the distribution of acceleration within session and across sessions
+
+# load dystonia database
+
+
+# across session and take the mean 
+
+    # single plot with all sessions in a specific time point overlayed in the same plot
+
+    # check mean
+
+# join all values from all files and check disctribution (what could we consider if there was a clear binomial distribution? could we use that treshold?)
+
+
+# try opening one of the created files with values and timestamps and plot 
